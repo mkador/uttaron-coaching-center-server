cc07c2a5edf80f2ce5e3f829db9afdd85018c02a
Mk Adore <mkador169@gmil.com>
2022-10-25 19:34:59 +0600

    testing and fake data link up

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
 
+app.use(cors())
+
+const courses = require('./data/courses.json')
+
+const coursesDetails = require('./data/courseDetails.json')
+
+app.get('/', (req, res) => {
+  res.send('Uttaron api running')
+})
+
+app.get('/courses', (req, res) => {
+  console.log(req.params.id)
+})
+
 app.listen(port, () => {
   console.log('Uttaron Coaching Center', port)
 })
